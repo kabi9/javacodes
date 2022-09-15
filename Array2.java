7d1fdff81f31696f57599bca3a8d9ba368793345
kabi9 <innocentkabi95@gmail.com>
2022-09-15 17:52:48 -0500

    array2

--- Array2.java
+++ Array2.java
@@ -33,9 +33,7 @@ public class Array2 {
 				//	0   1    2
 				// [11, 22, 33]
 		
-		//	PROGRAM 3:
-		
-		//	HOW TO ACCESS THE ELEMENTS FROM ARRAY.
+		//	PROGRAM 3: ACCESS THE ELEMENTS FROM ARRAY.
 		
 		//						0		1		 2		  3
 		String [] cities = {"pokhara","dharan","illam","damak"};
@@ -47,8 +45,76 @@ public class Array2 {
 		
 		int a = cities.length;
 		System.out.println(a);
+		System.out.println(cities[cities.length-1]);
 		
+		//	PROGRAM 4: PRINT EVERY ELEMENTS OF ARRAY
 		
+		//					   0		1		2			3
+		String [] fruits = {"apple", "mango", "banana", "chikoo"};
+		
+		for (int i = 0; i < 3; i++) {		// 	1	2	3
+			System.out.println(i);			//	0	1	2
+		}
+		
+		for (int i = 0; i < fruits.length; i++) {
+			System.out.println(fruits[i]);
+		}
+		
+		int [] numbers1 = {11,22,33,44,55};
+		
+		for (int i = 0; i < numbers1.length; i++) {
+			System.out.println(numbers1[i]);
+		}
+		
+		// PRINT ALL VALUES OF ARRAY IN REVERSE
+		
+		// 							0		1		 2			3
+		String [] vegetables = { "tomato","potato","Brinjal","cabbage" };
+		for (int i = vegetables.length-1; i >= 0; i--) {		//	2,1,0,-1
+			// System.out.println(i);							//	3,2,1,0
+			System.out.println(vegetables[i]);
+		}
+		
+		//PROGRAM 6:	USING FOR EACH LOOP
+		
+		int [] age = {11,22,33};
+		
+		for (int ag: age) {
+			System.out.println(ag);
+		}
+		
+		String [] countries = {"Pakistan","Nepal","India","China"};
+		
+		for (String country:countries) {
+			System.out.println(country);
+		}
+		
+		//	PROGRAM 7: SUM OF ALL NUMBERS IN ARRAY
+		
+		int [] numbers3 = {12,14,16};
+		int sum = 0;
+		 
+		for (int i = 0; i< numbers3.length; i++ ) {
+			sum = sum + numbers3[i];
+			//	0 	+ 	12	=	12
+			//	12	+	14	=	26
+			//	26	+	16	=	42
+		}
+			System.out.println(sum);
+		
+		// PROGRAM 8 : AVERAGE OF ALL ELEMENTS OF ARRAY
+			
+			int [] ages1 = {11,22,33,44};
+			int total = 0;		// sum of all elements = totalNumberOfElements
+			
+			for (int i = 0; i < ages1.length; i++) {
+				total = total + ages1[i];
+				//	0	+	11	=	11
+				//	11	+	22	=	33	
+				//	33	+	33	=	66	
+				//	66	+	44	=	110
+			}
+				System.out.println(total/ages1.length);
 	}
 
 }
